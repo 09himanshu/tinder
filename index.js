1a101eb50d9d33152bcf83f1ed49cfb4a1afecce
Himanshu Sah <09himanshusah@gmail.com>
2024-05-09 07:14:43 +0530

    do more task

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import fastify from 'fastify'
+import Fastify from 'fastify'
+import dotenv from 'dotenv'
+
+import {Dbservices} from './services/db.js'
+
+dotenv.config()
+
+async function init() {
+    const fastify = Fastify({logger: true})
+
+    let db_service = new Dbservices(process.env.url, process.env.db_name)
+
+    global.db_service = db_service
+}
--- services/db.js
+++ services/db.js
@@ -1,34 +1,155 @@
 import {MongoClient, ObjectId} from 'mongodb'
 
 class Dbservices {
-    #url;
-    #db_name;
-    #client;
+    
     #collection_names = []
-    #db
-
     constructor (url, db_name) {
         if(!Dbservices.instance) {
-            this.#url = url
-            this.#db_name = db_name
+           this.client = new MongoClient(url,{ useNewUrlParser: true, useUnifiedTopology: true })
+           this.connect()
+           this.db = this.client.db(db_name)
+           this.#collection_names = this.get_collections()
+           Dbservices.instance = this
         }
     }
-
-    async _ensure_connection() {
+    
+    async get_collections() {
         try {
-            if(!this.#client) {
-                this.#client = new MongoClient(this.#url, {
-                  useNewUrlParser: true,
-                  useUnifiedTopology: true,
-                })
-                // this.#client.
-            }
+            let collection = await this.db.listCollections().toArray()
+            // this.#collection_names = collection.map(ele => ele.name)
+            return collection
         } catch (err) {
             console.log(err);
         }
     }
 
     async connect() {
-        await this.#client.connect
+        return await this.client.connect()
+    }
+
+    async findMany(collection, filter, projection) {
+        try {
+            if(!this.#collection_names.includes(collection)) {
+                return {status: false, data: 'No collection found'}
+            }
+
+            let limit = 100
+            let skip = 0
+            let sort = {}
+
+            if(filter.limit) {
+                limit = filter.limit
+                delete filter.limit
+            }
+
+            if(filter.skip) {
+                skip = filter.skip
+                delete filter.skip
+            }
+
+            if(filter.sort) {
+                sort = filter.sort
+                delete filter.sort
+            }
+
+            let data = await this.db.collection(collection).find(filter || {}, projection || {}).skip(skip).limit(limit).sort(sort).toArray()
+            return {status: true, data}
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async findOne(collection, filter, projection) {
+        try {
+            if (!this.#collection_names.includes(collection)) {
+              return { status: false, data: 'No collection found' }
+            }
+
+            let data = await this.db.collection(collection).findOne(filter|| {}, projection || {})
+            return data
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async insertOne(collection, document) {
+        try {
+            if (!this.#collection_names.includes(collection)) {
+              await this.db.createCollection(collection)
+              this.#collection_names.push(collection)
+            }
+
+            let data = await this.db.collection(collection).insertOne(document)
+            return {status: true, data};
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async insertMany(collection, document) {
+        try {
+            if (!this.#collection_names.includes(collection)) {
+              await this.db.createCollection(collection)
+              this.#collection_names.push(collection)
+            }
+
+            let data = await this.db.collection(collection).insertMany(document)
+            return {status: true, data}
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async updateOne(collection, filter, update) {
+        try {
+            if (!this.#collection_names.includes(collection)) {
+              return { status: false, data: 'No collection found' }
+            }
+
+            let data = await this.db.collection(collection).updateOne(filter, update)
+            return {status: true, data}
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async updateMany(collection, filter, update) {
+        try {
+            if (!this.#collection_names.includes(collection)) {
+              return { status: false, data: 'No collection found' }
+            }
+
+            let data = await this.db.collection(collection).updateMany(filter, update)
+            return {status: true, data}
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async deleteOne(collection, filter) {
+        try {
+            if (!this.#collection_names.includes(collection)) {
+              return { status: false, data: 'No collection found' }
+            }
+
+            let data = await this.db.collection(collection).deleteOne(filter)
+            return data
+        } catch (err) {
+            console.log(err);
+        }
+    } 
+
+    async deleteMany(collection, filter) {
+        try {
+            if (!this.#collection_names.includes(collection)) {
+                return { status: false, data: 'No collection found' }
+            }
+            let data = await this.db.collection(collection).deleteMany(filter)
+            return {status: true, data}
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
+
+export {Dbservices}
